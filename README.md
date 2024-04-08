61aca3ec2a5ab5f9a2d31b76761114adf9879bf4
pradhul <pradhul.dev1990@gmail.com>
2024-04-08 18:05:27 +0530

    function for retriving log lives token

---
+++ README.md
@@ -0,0 +1,2 @@
+Netlify CLI - https://docs.netlify.com/cli/get-started/
+Running Builds locally - https://docs.netlify.com/cli/get-started/#run-builds-locally
---
+++ netlify/functions/longliveToken_IG.ts
@@ -0,0 +1,46 @@
+import type { Context } from "@netlify/functions"
+
+/**
+ * Get Long live token from instagram
+ * TODO: Remove maybe Explicit error messages on secrets like clientSecret is empty later
+ */
+
+export default async (req: Request, context: Context) => {
+  try {
+    /** Values From Request header */
+    const shortLivedToken = req.headers.get("X-ACCESS-TOKEN") || "";
+
+    /** Values From environement */
+    const grantType: string = Netlify.env.get("IG_GRANT_TYPE") || "";
+    const clientSecret: string = Netlify.env.get("IG_CLIENT_SECRET") || "";
+    const url: string = Netlify.env.get("IG_LONG_LIVE_TOKEN_URL") || "";
+
+    if (grantType === "") return new Response("Error: grantType is Empty");
+    if (clientSecret === "") return new Response("Error: clientSecret is Empty");
+    if (url === "") return new Response("Error: IG URL is Empty");
+    if (shortLivedToken === "") return new Response("Error: Did not recieve short lived token for exchange");
+
+    const params = new URLSearchParams();
+    params.append('grant_type', grantType);
+    params.append('client_secret', clientSecret)
+    params.append('access_token', shortLivedToken);
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-url-encoded' },
+      body: params
+    });
+
+    console.log("--------Request sent to: ", response.url);
+    console.log("--------Request sent with parameters: ", params.toString);
+
+    const data = await response.json();
+    return new Response(data);
+  } catch (error) {
+    console.error(error);
+    return new Response(error);
+  }
+
+}
+
+
